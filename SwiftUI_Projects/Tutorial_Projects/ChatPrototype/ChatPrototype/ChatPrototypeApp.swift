8924ea87595e7fff161e62a069edda2e6547861a
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-11-07 17:16:32 -0500

    feat: Section 4 Step 4 complete

---
+++ SwiftUI_Projects/Tutorial_Projects/ChatPrototype/ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Christopher Wilshusen on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
