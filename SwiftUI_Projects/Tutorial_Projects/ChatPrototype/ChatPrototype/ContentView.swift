531242895cbc1da0c0bf26560dad242c63342587
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-11-07 17:18:04 -0500

    feat: chapter 4 step 4 complete

--- SwiftUI_Projects/Tutorial_Projects/ChatPrototype/ChatPrototype/ContentView.swift
+++ SwiftUI_Projects/Tutorial_Projects/ChatPrototype/ChatPrototype/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Knock, knock!")
+                .padding()
+            Text("Who's there?")
         }
         .padding()
     }
@@ -22,3 +21,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
