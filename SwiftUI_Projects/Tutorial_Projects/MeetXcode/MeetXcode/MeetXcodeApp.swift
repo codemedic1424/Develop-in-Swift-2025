a77540d46eaec1c3945f212a404fa838b25cb2c6
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-11-07 10:23:23 -0500

    ADD: Meet Xcode project files.

---
+++ SwiftUI_Projects/Tutorial_Projects/MeetXcode/MeetXcode/MeetXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetXcodeApp.swift
+//  MeetXcode
+//
+//  Created by Christopher Wilshusen on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MeetXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
