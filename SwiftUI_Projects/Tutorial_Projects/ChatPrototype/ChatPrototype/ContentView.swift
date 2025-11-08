421ab848288f4f169f14369ad3147a6be35bc7c9
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-11-08 13:27:53 -0500

    Chat prototype complete.

--- SwiftUI_Projects/Tutorial_Projects/ChatPrototype/ChatPrototype/ContentView.swift
+++ SwiftUI_Projects/Tutorial_Projects/ChatPrototype/ChatPrototype/ContentView.swift
@@ -12,7 +12,10 @@ struct ContentView: View {
         VStack {
             Text("Knock, knock!")
                 .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
             Text("Who's there?")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
         }
         .padding()
     }
