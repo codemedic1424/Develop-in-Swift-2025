a0f8fa3eb1f8064151396b3c1e15844a83762d64
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-11-08 10:13:17 -0500

    DOCS: section 2 complete, begin section 3

--- SwiftUI_Projects/Tutorial_Projects/WeatherForecast/WeatherForecast/ContentView.swift
+++ SwiftUI_Projects/Tutorial_Projects/WeatherForecast/WeatherForecast/ContentView.swift
@@ -9,12 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Mon")
-            Text("High: 70")
-            Text("Low: 50")
+        HStack {
+            VStack {
+                Text("Mon")
+                Image(systemName: "sun.max.fill")
+                    .foregroundStyle(.yellow)
+                Text("High: 70")
+                Text("Low: 50")
+            }
+            .padding()
+            
+            VStack {
+                Text("Tues")
+                Image(systemName: "cloud.rain.fill")
+                    .foregroundStyle(.blue)
+                Text("High: 60")
+                Text("Low: 40")
+            }
         }
-        .padding()
     }
 }
 
