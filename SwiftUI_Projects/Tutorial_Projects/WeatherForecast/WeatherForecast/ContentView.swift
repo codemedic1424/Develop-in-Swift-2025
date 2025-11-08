4df0ded9426b1c684e7628c2b2cc77b3c86714fe
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-11-08 12:45:10 -0500

    Customize views with properties tutorial complete.

--- SwiftUI_Projects/Tutorial_Projects/WeatherForecast/WeatherForecast/ContentView.swift
+++ SwiftUI_Projects/Tutorial_Projects/WeatherForecast/WeatherForecast/ContentView.swift
@@ -11,11 +11,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            DayForecast(day: "Mon", low: 50, high: 70, isRaining: false)
-            
-            DayForecast(day: "Tues", low: 40, high: 60, isRaining: true)
+        HStack{
+            Button("Prev Week", systemImage: "chevron.backward") {
+                //TODO: Add button action
+            }
+            Spacer()
+            Button {
+                //TODO: Add button action
+            } label: {
+                HStack(spacing: 6) {
+                    Text(" Next Week")
+                    Image(systemName: "chevron.forward")
+                }
+            }
         }
+        .padding()
+        
+        
+        Text("Weekly Weather Forecast")
+            .font(.title)
+        VStack {
+            HStack {
+                DayForecast(day: "Mon", low: 50, high: 70, isRaining: false)
+                
+                DayForecast(day: "Tues", low: 40, high: 60, isRaining: true)
+            }
+            HStack {
+                DayForecast(day: "Wed", low: 46, high: 72, isRaining: false)
+                
+                DayForecast(day: "Thurs", low: 56, high: 76, isRaining: false)
+            }
+            HStack {
+                DayForecast(day: "Fri", low: 54, high: 80, isRaining: false)
+                
+                DayForecast(day: "Sat", low: 42, high: 64, isRaining: true)
+            }
+            HStack {
+                DayForecast(day: "Sun", low: 40, high: 62, isRaining: true)
+            }
+        }
+        
     }
 }
 struct DayForecast: View {
@@ -43,13 +78,19 @@ struct DayForecast: View {
     }
     
 // MARK: View Layout
+    //TODO: adjust spacing and add background tinting
     var body: some View {
         VStack {
             Text(day)
+                .font(.headline)
             Image(systemName: iconName)
                 .foregroundStyle(iconColor)
+                .font(.title)
+                .padding(.bottom, 5)
             Text("High \(high)º")
+                .fontWeight(.semibold)
             Text("Low: \(low)º")
+                .foregroundStyle(.secondary)
         }
         .padding()
     }
