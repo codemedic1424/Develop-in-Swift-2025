e706ed6b6d01fb75c73b46fee37391d1a30d9ef6
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-11-08 11:17:26 -0500

    section 7 complete

--- SwiftUI_Projects/Tutorial_Projects/WeatherForecast/WeatherForecast/ContentView.swift
+++ SwiftUI_Projects/Tutorial_Projects/WeatherForecast/WeatherForecast/ContentView.swift
@@ -7,26 +7,47 @@
 
 import SwiftUI
 
+// MARK: View Construction
+
 struct ContentView: View {
     var body: some View {
         HStack {
-            DayForecast(day: "Mon", low: 50, high: 70)
+            DayForecast(day: "Mon", low: 50, high: 70, isRaining: false)
             
-            DayForecast(day: "Tues", low: 40, high: 60)
+            DayForecast(day: "Tues", low: 40, high: 60, isRaining: true)
         }
     }
 }
 struct DayForecast: View {
+    // MARK: Properties
+    
     let day: String
     let low: Int
     let high: Int
-    //let weatherImage: Image
+    let isRaining: Bool
     
+    var iconName: String {
+        if isRaining {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRaining {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
+    
+// MARK: View Layout
     var body: some View {
         VStack {
             Text(day)
-            Image(systemName: "sun.max.fill")
-                .foregroundStyle(.yellow)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
             Text("High \(high)º")
             Text("Low: \(low)º")
         }
