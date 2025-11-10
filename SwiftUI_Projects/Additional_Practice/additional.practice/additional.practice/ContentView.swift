2859b593009ad431145e02163796daca6df87aaf
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-11-10 11:30:19 -0500

    Initial commit - project workspace init.

---
+++ SwiftUI_Projects/Additional_Practice/additional.practice/additional.practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  additional.practice
+//
+//  Created by Christopher Wilshusen on 11/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Projects/Additional_Practice/additional.practice/additional.practice/additional_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  additional_practiceApp.swift
+//  additional.practice
+//
+//  Created by Christopher Wilshusen on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct additional_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
