45df568a390145db3389b77b8258e721d455eb80
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-11-11 16:37:49 -0500

    doc: Create Lab 3

---
+++ SwiftUI_Projects/Additional_Practice/Lab_3_HabitTracker/Lab_3_HabitTracker/Lab_3_HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab_3_HabitTrackerApp.swift
+//  Lab_3_HabitTracker
+//
+//  Created by Christopher Wilshusen on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Lab_3_HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
