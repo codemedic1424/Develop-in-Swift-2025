2765b2770fe27eb2521be3bbcd37c9d829845ef9
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-11-13 11:48:15 -0500

    Section 4 Complete

--- SwiftUI_Projects/Additional_Practice/Computed_Properties_Practice_SwiftUI/Computed_Properties_Practice_SwiftUI/Section4.swift
+++ SwiftUI_Projects/Additional_Practice/Computed_Properties_Practice_SwiftUI/Computed_Properties_Practice_SwiftUI/Section4.swift
@@ -11,40 +11,68 @@ import SwiftUI
 // ==========================================================
 
 
-// Exercise 10 – Dynamic Icon Name
-// Computed property favoriteIconName -> "star" or "star.fill"
-
-// Exercise 11 – Card Background Color
-// Computed property cardBackground -> green or gray opacity
-
 //MARK: Exercise 9 – Button Title Based on State
 // @State var isPlaying: Bool
 // Computed property buttonTitle -> "Play"/"Pause"
 struct Section4: View {
-    //MARK: E9 Variables
+    //MARK: E9 Variables:
     @State var isPlaying: Bool = false
     
     var buttonTitle: String {
-        if isPlaying {
-            return "Play"
-        } else {
-            return "Pause"
-        }
+        isPlaying ? "Pause" : "Play"
+    }
+    var buttonImage: String {
+        isPlaying ? "pause.fill" : "play.fill"
     }
     func stateToggle() {
         isPlaying.toggle()
     }
+    //MARK: E10 Variables:
+    @State var isFavorite: Bool = false
+    
+    var favoriteIcon: String {
+        isFavorite ? "star.fill" : "star"
+    }
+    func buttonChange() {
+        isFavorite.toggle()
+    }
+    //MARK: E11 Properties
+    var colorBackground: Color {
+        isPlaying ? .green : .gray
+    }
     
     var body: some View {
-        Button(buttonTitle) {
-            stateToggle()
+        ZStack {
+            colorBackground.ignoresSafeArea().opacity(0.5)
+            HStack {
+                Button {
+                    stateToggle()
+                } label: {
+                    Label(buttonTitle, systemImage: buttonImage)
+                }
+                .buttonStyle(.glass)
+                .shadow(radius: 5)
+                
+                
+                //MARK: -Exercise 10 – Dynamic Icon Name
+                // Computed property favoriteIconName -> "star" or "star.fill"
+                Button {
+                    buttonChange()
+                } label: {
+                    Image(systemName: favoriteIcon)
+                }
+                .buttonStyle(.glass)
+                .shadow(radius: 05)
+            }
+           
         }
-       
-      
-
         
+        //MARK:  Exercise 11 – Card Background Color
+        // Computed property cardBackground -> green or gray opacity
     }
+        
 }
+
 
 #Preview {
     Section4()
