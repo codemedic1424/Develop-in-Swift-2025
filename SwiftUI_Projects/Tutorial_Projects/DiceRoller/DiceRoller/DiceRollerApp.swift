67fb850714b5822c0881066537cc1f3c5d172dff
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-11-18 16:39:12 -0500

    Chapter4 Section 2 'DiceRoller' State and Button Begin

---
+++ SwiftUI_Projects/Tutorial_Projects/DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Christopher Wilshusen on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
