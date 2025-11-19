854430f3cf64462ad52c6f3ecf1c2d1a6d66d057
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-11-19 13:49:58 -0500

    Complete SwiftUI Tutorial Chapter 5 - Pick-a-Pal #13

---
+++ SwiftUI_Projects/Tutorial_Projects/Pick-a-Pal/Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Christopher Wilshusen on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
