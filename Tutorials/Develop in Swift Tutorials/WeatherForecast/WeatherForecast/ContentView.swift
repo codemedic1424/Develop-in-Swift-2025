ff49e5a56d0c1f72aeb25664ab9087df7e1062d0
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-12-06 05:17:29 -0500

    docs: completed Wrap-up: Views, structures, and properties Article

--- Tutorials/Develop in Swift Tutorials/WeatherForecast/WeatherForecast/ContentView.swift
+++ Tutorials/Develop in Swift Tutorials/WeatherForecast/WeatherForecast/ContentView.swift
@@ -9,10 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            DayForecast(dayOfWeek: "Mon", isRainy: false, high: 70, low: 50)
-            
-            DayForecast(dayOfWeek: "Tue", isRainy: true, high: 65, low: 48)
+        Text("Weekly Weather")
+            .font(.title)
+            .underline()
+            .fontWeight(.bold)
+            .foregroundStyle(.secondary)
+        Text("12/08/2025")
+            .font(.caption)
+            .padding(.top, 0.5)
+        VStack{
+            HStack {
+                DayForecast(dayOfWeek: "Mon", isRainy: false, high: 70, low: 50)
+                
+                DayForecast(dayOfWeek: "Tue", isRainy: true, high: 65, low: 48)
+            }
+            HStack {
+                DayForecast(dayOfWeek: "Wed", isRainy: false, high: 70, low: 48)
+                
+                DayForecast(dayOfWeek: "Thu", isRainy: false, high: 78, low: 62)
+            }
+            HStack {
+                DayForecast(dayOfWeek: "Fri", isRainy: false, high: 70, low: 50)
+                
+                DayForecast(dayOfWeek: "Sat", isRainy: true, high: 65, low: 48)
+            }
+            HStack {
+                DayForecast(dayOfWeek: "Sun", isRainy: true, high: 70, low: 55)
+                WeekForecast(rainyDays: 3)
+            }
         }
     }
 }
@@ -48,6 +72,37 @@ struct DayForecast: View {
                 .fontWeight(low < 50 ? .bold : .regular)
         }
         .padding()
+        .background(in: RoundedRectangle(cornerRadius: 5))
+        .backgroundStyle(.quinary)
+        .padding(6)
+    }
+}
+
+//TODO: Finish logic for below
+//Basic structure added
+
+struct WeekForecast: View {
+    let highs = [70, 65, 70, 78, 70, 65, 70]
+    let lows = [50, 48, 48, 62, 50, 48, 55]
+    var highAvg: Int {
+        highs.reduce(0, +)/highs.count
+    }
+    var lowAvg: Int {
+        lows.reduce(0,+)/lows.count
+    }
+    let rainyDays: Int
+    
+    
+    var body: some View {
+        VStack {
+            Text("Weather View")
+                .font(.title3)
+                .fontWeight(.bold)
+                .padding(2)
+            Text("Avg. High: \(highAvg)ºF")
+            Text("Avg. Low: \(lowAvg)ºF")
+            Text("# of Rainy Days: ")
+        }
     }
 }
 
