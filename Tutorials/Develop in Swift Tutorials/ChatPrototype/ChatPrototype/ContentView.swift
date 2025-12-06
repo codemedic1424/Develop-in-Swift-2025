e2047ca83fda62671b3e13daa8ff48a483f7c15a
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-12-06 01:59:07 -0500

    docs: Meet SwiftUI - ChatPrototype Complete

--- Tutorials/Develop in Swift Tutorials/ChatPrototype/ChatPrototype/ContentView.swift
+++ Tutorials/Develop in Swift Tutorials/ChatPrototype/ChatPrototype/ContentView.swift
@@ -9,11 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 25) {
+            Text("Knock, knock!")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 12)).shadow(radius: 6)
+            
+            Text("Who's there?")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 12))
+                .shadow(radius: 6)
+            
+            Text("Dozen.")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 12))
+                .shadow(radius: 6)
+            
+            Text("Dozen who?")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 12))
+                .shadow(radius: 6)
+            
+            Text("Dozen matter, let me in!")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 12))
+                .shadow(radius: 6)
         }
         .padding()
     }
