77643d00c5e95e14a569fe048b1d1c506e37952b
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-12-16 09:50:55 -0500

    docs: update UI with stat tutorial initial commit

---
+++ Tutorials/Develop in Swift Tutorials/DiceRoler/DiceRoler/DiceRolerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRolerApp.swift
+//  DiceRoler
+//
+//  Created by Christopher Wilshusen on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceRolerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tutorials/Develop in Swift Tutorials/DiceRoler/DiceRoler/DiceView.swift
@@ -0,0 +1,18 @@
+//
+//  DiceView.swift
+//  DiceRoler
+//
+//  Created by Christopher Wilshusen on 12/16/25.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiceView()
+}
