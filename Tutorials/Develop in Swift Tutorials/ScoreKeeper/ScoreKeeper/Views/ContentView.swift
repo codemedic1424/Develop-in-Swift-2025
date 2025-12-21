8e6174bb2304c997f597c48c2e97c23632456daa
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-12-21 01:31:09 -0500

    feat: add streamlines UI and list ordering

--- Tutorials/Develop in Swift Tutorials/ScoreKeeper/ScoreKeeper/Views/ContentView.swift
+++ Tutorials/Develop in Swift Tutorials/ScoreKeeper/ScoreKeeper/Views/ContentView.swift
@@ -15,13 +15,19 @@ struct ContentView: View {
         Player(name: "Linda", score: 0)
     ]
     @State private var editMode: EditMode = .inactive
-
+    
     private var isEditing: Bool {
         editMode.isEditing
     }
     
     func movePlayers(from source: IndexSet, to destination: Int) {
         players.move(fromOffsets: source, toOffset: destination)
+    }
+    
+    func deletePlayer(id: UUID) {
+        if let index = players.firstIndex(where: { $0.id == id }) {
+            players.remove(at: index)
+        }
     }
     
     var body: some View {
@@ -39,26 +45,38 @@ struct ContentView: View {
                             
                             Spacer()
                             
-                            Text("\(player.score)")
-                                .monospacedDigit()
+                            if !isEditing {
+                                Text("\(player.score)")
+                                    .monospacedDigit()
+                            }
                             
-                            Stepper("", value: $player.score)
-                                .labelsHidden()
+                            if isEditing {
+                                Button(role: .destructive) {
+                                    deletePlayer(id: player.id)
+                                } label: {
+                                    Image(systemName: "trash")
+                                }
+                                .buttonStyle(.borderless)
+                            } else {
+                                Stepper("", value: $player.score)
+                                    .labelsHidden()
+                            }
                         }
                     }
                     .onMove(perform: movePlayers)
                 }
                 
-//                Section {
-//                    if isEditing {
-//                        Button("Add Player", systemImage: "plus") {
-//                            players.append(Player(name: "", score: 0))
-//                        }
-//                    }
-//                }
+                Section {
+                    if isEditing {
+                        Button("Add Player", systemImage: "plus") {
+                            players.append(Player(name: "", score: 0))
+                        }
+                    }
+                }
                 
                 Section {
-                    HStack(alignment: .center) {
+                    HStack() {
+                        Spacer()
                         Button("Reset Scores") {
                             for i in players.indices {
                                 players[i].score = 0
@@ -66,6 +84,7 @@ struct ContentView: View {
                         }
                         .tint(.red.opacity(0.8))
                         .buttonStyle(.glassProminent)
+                        Spacer()
                     }
                 }
             }
