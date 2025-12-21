d3a7c4f33a15a1a7ddaa3290f39da021e5ce0239
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-12-21 02:55:44 -0500

    chore: initial commit - Add func with swift testing

---
+++ Tutorials/Develop in Swift Tutorials/ScoreKeeper2/ScoreKeeper/Scoreboard.swift
@@ -0,0 +1,16 @@
+//
+//  Scoreboard.swift
+//  ScoreKeeper
+//
+//  Created by Christopher Wilshusen on 12/21/25.
+//
+
+import Foundation
+
+struct Scoreboard {
+    var players: [Player] = [
+        Player(name: "Elisha", score: 0),
+        Player(name: "Andre", score: 0),
+        Player(name: "Jasmine", score: 0)
+        ]
+}
