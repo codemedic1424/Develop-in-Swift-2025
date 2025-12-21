37109aa48da090da5f71747266b280c5c7d43476
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-12-20 21:54:32 -0500

    feat: list reorder pause

--- Tutorials/Develop in Swift Tutorials/ScoreKeeper/ScoreKeeper/Views/ContentView.swift
+++ Tutorials/Develop in Swift Tutorials/ScoreKeeper/ScoreKeeper/Views/ContentView.swift
@@ -14,61 +14,57 @@ struct ContentView: View {
         Player(name: "Theo", score: 0),
         Player(name: "Linda", score: 0)
     ]
-   
+    
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Score Keeper")
-                .font(.title)
-                .bold()
-                .padding(.bottom)
-            
-            Grid {
-                GridRow {
-                    Text("Player")
-                        .gridColumnAlignment(.leading)
-                    Text("Score")
-                }
-                .font(.headline)
-                
-                ForEach($players) { $player in
-                    GridRow {
-                        TextField("Name", text: $player.name)
+        
+        Text("Score Keeper")
+            .font(.title)
+            .bold()
+            .padding(.bottom)
+        
+        EditButton()
+        
+        NavigationStack {
+// TODO: Finish list reorder logic
+                List {
+                    ForEach($players) { $player in
+                        Text("\(player.name)")
                         
-                        Text("\(player.score)")
-                        
-                        Stepper("\(player.score)", value: $player.score)
-                            .labelsHidden()
+                        //  Text("\(player.score)")
+                        //  Stepper("\(player.score)", value: $player.score)
+                        //    .labelsHidden()
                     }
                 }
-            }
-            .padding(.vertical)
-            
-            Button("Add Player", systemImage: "plus") {
-                players.append(Player(name: "", score: 0))
-            }
-            .autocorrectionDisabled()
-            
-            HStack(alignment: .center) {
+                .onMove(perform: <#T##Optional<(Foundation.IndexSet, Int) -> Void>#>)
+                
+                .padding(.vertical)
+                
+                Button("Add Player", systemImage: "plus") {
+                    players.append(Player(name: "", score: 0))
+                }
+                .autocorrectionDisabled()
+                
+                HStack(alignment: .center) {
+                    Spacer()
+                    Button("Reset Scores") {
+                        for i in players.indices {
+                            players[i].score = 0
+                        }
+                    }
+                    .tint(.red.opacity(0.8))
+                    .buttonStyle(.glassProminent)
+                    .padding(.top)
+                    
+                    Spacer()
+                }
+                
+                
                 Spacer()
-                Button("Reset Scores") {
-                    for i in players.indices {
-                        players[i].score = 0
-                    }
-                }
-                .tint(.red.opacity(0.8))
-                .buttonStyle(.glassProminent)
-                .padding(.top)
-                
-            Spacer()
             }
-            
-            
-            Spacer()
+            .padding()
         }
-        .padding()
     }
-}
 
 #Preview {
     ContentView()
