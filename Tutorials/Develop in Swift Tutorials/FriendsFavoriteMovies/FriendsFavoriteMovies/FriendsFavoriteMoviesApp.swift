e415bce06a205747439449ade27ecef56474259e
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-12-25 20:12:57 -0500

    chore: initial commit

---
+++ Tutorials/Develop in Swift Tutorials/FriendsFavoriteMovies/FriendsFavoriteMovies/FriendsFavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesApp.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Christopher Wilshusen on 12/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
